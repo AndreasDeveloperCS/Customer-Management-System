0f3a08b143e07ce62008360a5e837ab7927b7175
AndreasDeveloperCS <AandreasPetrov@gmail.com>
2022-04-17 12:28:21 +0300

    UT

--- CMS/CMS.Data.Models.UnitTests/CustomerDtoUnitTests.cs
+++ CMS/CMS.Data.Models.UnitTests/CustomerDtoUnitTests.cs
@@ -1,5 +1,7 @@
-
+using CMS.Data.Models.DTOs;
 using NUnit.Framework;
+using System;
+using System.Linq;
 
 namespace CMS.Data.Models.UnitTests
 {
@@ -10,11 +12,52 @@ namespace CMS.Data.Models.UnitTests
     public class CustomerDtoUnitTests
     {
         [Test]
-        public void TestMethod1()
+        public void CustomerDto_Instance_CreatedSuccessfully()
         {
+            var customer = new CustomerDto()
+            {
+                Id = 1,
+                FirstName = "FirstName1",
+                LastName = "LastName1",
+                PostalCode = "12345",
+                Address = "Adress1"
+            };
 
+            var product = new ProductDto()
+            {
+                Id = 1,
+                Name = "Product 1",
+                PricePerUnit = 10,
+                MeasuringUnit = "per/unit"
+            };
+            var item = new ItemDto
+            {
+                Id = 1,
+                Quantity = 100,
+                OrderId = 1,
+                ProductId = 1,
+                Product = product
+            };
+            var items = new ItemDto[1]
+            {
+                item
+            };
+            var order = new OrderDto()
+            {
+                Id = 1,
+                CustomerId = 1,
+                Customer = customer,
+                OrderDate = DateTime.Now,
+                TotalPrice = 200.0m,
+                Items = items
+            };
+            item.Order = order;
+
+            Assert.NotNull(order.Items);
+            Assert.NotNull(order.Items.First().Order);
+            Assert.AreEqual(1, order.Items.Count());
+            Assert.AreEqual(1, order.Items.First().Product);
+            Assert.AreEqual(1, order.Items.First());
         }
-
-
     }
 }
--- CMS/CMS.Data.Models.UnitTests/OrderDtoUnitTests.cs
+++ CMS/CMS.Data.Models.UnitTests/OrderDtoUnitTests.cs
@@ -1,14 +1,31 @@
-
+using CMS.Data.Models.DTOs;
 using NUnit.Framework;
+using System.Linq;
 
 namespace CMS.Data.Models.UnitTests
 {
+    /// <summary>
+    /// OrderDtoUnitTests covers the OrderDto with Unit tests 
+    /// </summary>
     [TestFixture]
     public class OrderDtoUnitTests
     {
         [Test]
-        public void TestMethod1()
+        public void OrderDto_Instance_CreatedSuccessfully()
         {
+            var orders = new OrderDto[1] { new OrderDto() };
+            var customer = new CustomerDto()
+            {
+                Id = 1,
+                FirstName = "FirstName1",
+                LastName = "LastName1",
+                PostalCode = "12345",
+                Address = "Adress1",
+                Orders = orders
+            };
+
+            Assert.NotNull(customer.Orders);
+            Assert.AreEqual(1, customer.Orders.Count());
         }
     }
 }
